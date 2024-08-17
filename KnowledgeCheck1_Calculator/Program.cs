b8ab3bc60f8a6247b4efcd4fdbef24a87388dba6
rdanilowicz <rjdanilowicz@gmail.com>
2024-08-17 14:22:07 -0400

    Commiting refactoring changes to the code.

--- KnowledgeCheck1_Calculator/Program.cs
+++ KnowledgeCheck1_Calculator/Program.cs
@@ -12,7 +12,8 @@ namespace KnowledgeCheck1_Calculator
         {
 
             Console.WriteLine("Hello. Press 1 for addition, 2 for subtraction, 3 for multiplication, and 4 for division");
-            //Moved the methods for each action to their corresponding method in the calculator class. Each switch statement now calls a constructor for each depending on the input. 
+            //Moved methods for calculation of addition, subtraction, multiplipication and division into the corresponding method in calculator class.
+            var input = Console.ReadLine();
             var calculator = new Calculator();
 
             switch (input)
